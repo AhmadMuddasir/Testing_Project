ccccd074b3c1b4cc5c340c225a4042a9a4a86840
CodeReviser <ahmadmuddasir0786@gmail.com>
2026-03-29 11:02:20 +0530

    navigation bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,15 @@
-import React from 'react'
+'use client'
 import Link from 'next/link'
+import { useAuth } from '@/context/AuthContext'
 
 const Navbar = () => {
   return (
-    <div className='flex items-center justify-around bg-gray-900 w-full'>
-     <div className=''>
-          logo
-     </div>
-     <div className="">
-          <nav className="">
-               <ul className="capitalize flex p-2 m-2 justify-around w-full">
-                    <li className='' Link="/">Home</li>
-                    <li className='' Link="/about">about</li>
-                    <li className='' Link="/#">service</li>
-                    <li className='' Link="/login">login</li>
-                    <li className='' Link="/register">login</li>
-               </ul>
-          </nav>
-     </div>
-    </div>
+     <nav className='bg-blue-600'>
+          <div className="">
+               <Link href={`/`} className='hover-underline' >
+               </Link>
+          </div>
+     </nav>
   )
 }
 
